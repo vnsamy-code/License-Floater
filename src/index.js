8dd03618c351d63b15d7264c810fa105b6699268
Vignesh, N <vigneshnv7@gmail.com>
2022-05-11 06:17:19 +0200

    Pass environment in start script in package.json.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-console.log('nodemon test...');
+const dotenv = require('dotenv');
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+
+// console.log(`PROD VALUE: ${process.env.TEST}`);
+console.log(`DEV VALUE: ${process.env.DEPLOY_SECRET}`);
 console.log('nodemon another test...');
+console.log(`${process.env.NODE_ENV}`);
